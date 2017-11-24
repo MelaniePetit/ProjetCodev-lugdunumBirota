641edd960100f1757c35b6ea8e4a4447d7afd27f
Collin Léa <collin.lea@hotmail.fr>
2017-11-24 10:55:51 +0100

    Modification titre

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, ViewChild } from '@angular/core';
 
 import { Platform, MenuController, Nav } from 'ionic-angular';
 
-import { HelloIonicPage } from '../pages/hello-ionic/hello-ionic';
+import { AccueilPage } from '../pages/accueil/accueil';
 import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -16,7 +16,7 @@ export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
   // make HelloIonicPage the root (or first) page
-  rootPage = HelloIonicPage;
+  rootPage =  AccueilPage;
   pages: Array<{title: string, component: any}>;
 
   constructor(
@@ -29,8 +29,8 @@ export class MyApp {
 
     // set our app's pages
     this.pages = [
-      { title: 'Hello Ionic', component: HelloIonicPage },
-      { title: 'My First List', component: ListPage }
+      { title: 'Accueil', component: AccueilPage },
+      { title: 'Liste d éléments', component: ListPage }
     ];
   }
 
---
+++ src/pages/accueil/accueil.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'page-accueil',
+  templateUrl: 'accueil.html'
+})
+export class AccueilPage {
+  constructor() {
+
+  }
+}
