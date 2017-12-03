d69be893d9a6e37627d65158ca7dd90338151291
PETITCUENOT Mélanie <melanie.petitcuenot@etu.univ-lyon1.fr>
2017-12-03 21:23:09 +0100

    Emplacement des stations
    Récupération des données sur le Grand Lyon.
    Traitement des données récupérées.
    Affichage de simples points à l'emplacement de chaque station

--- src/pages/accueil/accueil.ts
+++ src/pages/accueil/accueil.ts
@@ -1,33 +1,69 @@
 import { Component } from '@angular/core';
 import ol from 'openlayers';
+import { Http } from '@angular/http'
+import 'rxjs/add/operator/map';
+
+import { Station } from '../../model/station'
+
 
 @Component({
   selector: 'page-accueil',
   templateUrl: 'accueil.html'
 })
+
 export class AccueilPage {
-
+  stations : Station[] = [];    
   map;
+  markers = [];
 
-  constructor() {
-
+  constructor(public http: Http) {
+    this.http.get('https://download.data.grandlyon.com/ws/rdata/jcd_jcdecaux.jcdvelov/all.json').map(res => res.json()).subscribe(data => {
+      this.stations = data.values;  
+      this.createMap()      
+      
+    });
   }
 
-  ionViewDidLoad() {
+  createMap() {
+    for(let i = 0; i < this.stations.length ; i++){
+      let marker = new ol.Feature({
+        geometry: new ol.geom.Point(ol.proj.fromLonLat([+this.stations[i].lng, +this.stations[i].lat]))
+      });
+      
+      marker.setStyle(new ol.style.Style({
+        image: new ol.style.Icon(/** @type {olx.style.IconOptions} */ ({
+          crossOrigin: 'anonymous',
+          src: 'https://openlayers.org/en/v4.5.0/examples/data/dot.png'
+        }))
+      }));
+
+      this.markers.push(marker);
+    }
+
+    let vectorSource = new ol.source.Vector({
+      features: this.markers,
+    });
+
+    let vectorLayer = new ol.layer.Vector({
+      source: vectorSource
+    });
+    
     this.map = new ol.Map({
       target: "map",
+
       layers: [
         new ol.layer.Tile({
           source: new ol.source.XYZ({
             url: "https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}/?access_token=pk.eyJ1IjoibWVsYW5pZWxlYSIsImEiOiJjamFkczVuMmgwbm5vMzJvaTY4ZmU2YnhuIn0.SfQ-NCHeu7WgeehCuqMjvA"
           })
-        })
+        }),
+        vectorLayer,
       ],
+
       view: new ol.View({
-        center: ol.proj.transform([4.868345, 45.779324], 'EPSG:4326','EPSG:3857'),
-        zoom: 15
-      }),
-      controls: []
+        center: ol.proj.fromLonLat([4.868345, 45.779324]),
+        zoom: 10
+      })
     })
   }
 }
--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,34 +1,20 @@
 import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http'
+import 'rxjs/add/operator/map';
 
-import { NavController, NavParams } from 'ionic-angular';
 
-import { ItemDetailsPage } from '../item-details/item-details';
 
 @Component({
   selector: 'page-list',
   templateUrl: 'list.html'
 })
 export class ListPage {
-  icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+  stations = [];  
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
-
-    this.items = [];
-    for(let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
-  }
-
-  itemTapped(event, item) {
-    this.navCtrl.push(ItemDetailsPage, {
-      item: item
+  constructor(public navCtrl: NavController, public http: Http) {
+    this.http.get('https://download.data.grandlyon.com/ws/rdata/jcd_jcdecaux.jcdvelov/all.json').map(res => res.json()).subscribe(data => {
+      this.stations = data.values;      
     });
   }
 }
