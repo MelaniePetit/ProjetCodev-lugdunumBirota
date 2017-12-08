564e893c1ab85fc9b7fe5b5adc9b9027e73b9f3d
Collin Léa <collin.lea@hotmail.fr>
2017-12-08 11:33:44 +0100

    Affichage marqueurs + cluster

--- src/pages/accueil/accueil.ts
+++ src/pages/accueil/accueil.ts
@@ -29,13 +29,6 @@ export class AccueilPage {
       let marker = new ol.Feature({
         geometry: new ol.geom.Point(ol.proj.fromLonLat([+this.stations[i].lng, +this.stations[i].lat]))
       });
-      
-      marker.setStyle(new ol.style.Style({
-        image: new ol.style.Icon(/** @type {olx.style.IconOptions} */ ({
-          crossOrigin: 'anonymous',
-          src: 'https://openlayers.org/en/v4.5.0/examples/data/dot.png'
-        }))
-      }));
 
       this.markers.push(marker);
     }
@@ -44,21 +37,58 @@ export class AccueilPage {
       features: this.markers,
     });
 
-    let vectorLayer = new ol.layer.Vector({
+    let clusterSource = new ol.source.Cluster({
+      distance: 30,
       source: vectorSource
     });
-    
+
+    let styleCache = {};    
+    let vectorLayer = new ol.layer.Vector({
+      source: clusterSource,
+      style: function(feature) {
+        var size = feature.get('features').length;
+        var style = styleCache[size];
+        if (!style) {
+          if (size > 1){
+            style = new ol.style.Style({
+              image: new ol.style.Icon(({
+                src: '../../assets/imgs/logo.png',
+                scale: 0.075
+              })),
+              text: new ol.style.Text({
+                text: size.toString(),
+                fill: new ol.style.Fill({
+                  color: '#fff'
+                })
+              })
+            });
+          }
+          else {
+            style = new ol.style.Style({
+              image: new ol.style.Icon(({
+                src: '../../assets/imgs/little_icon.png',
+                scale: 0.5
+              })),
+            });
+              
+          }
+          
+          styleCache[size] = style;
+        }
+        return style;
+      }
+    });    
+
+    var mapSource = new ol.layer.Tile({
+      source: new ol.source.XYZ({
+        url: "https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}/?access_token=pk.eyJ1IjoibWVsYW5pZWxlYSIsImEiOiJjamFkczVuMmgwbm5vMzJvaTY4ZmU2YnhuIn0.SfQ-NCHeu7WgeehCuqMjvA"
+      })
+    });
+
     this.map = new ol.Map({
       target: "map",
 
-      layers: [
-        new ol.layer.Tile({
-          source: new ol.source.XYZ({
-            url: "https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}/?access_token=pk.eyJ1IjoibWVsYW5pZWxlYSIsImEiOiJjamFkczVuMmgwbm5vMzJvaTY4ZmU2YnhuIn0.SfQ-NCHeu7WgeehCuqMjvA"
-          })
-        }),
-        vectorLayer,
-      ],
+      layers: [mapSource,vectorLayer],
 
       view: new ol.View({
         center: ol.proj.fromLonLat([4.868345, 45.779324]),
